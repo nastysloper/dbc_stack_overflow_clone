b9e09012069c845f27fc991460ac26a1171d26eb
Torey Hickman <torey@toreyhickman.com>
2013-07-17 18:26:01 -0500

    user model and migration

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+
+  has_many :created_events, :class_name => "Event", :foreign_key => :organizer_id
+  has_many :attendees
+  has_many :attended_events, :through => :attendees, :source => :events
+  has_many :votes, :foreign_key => :voter_id
+  has_many :comments, :foreign_key => :author_id
+  has_many :voted_on_comments, :through => :votes, :source => :comments
+
+
+  def twitter_client
+    @twitter_client ||= Twitter::Client.new(:oauth_token => oauth_token,
+                                            :oauth_token_secret => oauth_secret)
+  end
+
+end
--- db/migrate/20130717225858_create_users.rb
+++ db/migrate/20130717225858_create_users.rb
@@ -1,5 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def up
+    create_table :users do |t|
+      t.string :oauth_token
+      t.string :oauth_secret
+
+      t.timestamps
   end
 
   def down
