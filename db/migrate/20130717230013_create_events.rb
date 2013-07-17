118345dc6d3fcda104d18845f16d68b40da34299
Torey Hickman <torey@toreyhickman.com>
2013-07-17 18:59:51 -0500

    event model and migration complete, carrierwave PhotoUploader class added to handle event image

--- db/migrate/20130717230013_create_events.rb
+++ db/migrate/20130717230013_create_events.rb
@@ -1,5 +1,16 @@
 class CreateEvents < ActiveRecord::Migration
+  
   def up
+    create_table :events do |t|
+      t.string :title
+      t.text :description
+      t.datetime :start
+      t.datetime :end
+      t.string :photo_file
+      t.integer :organizer_id
+
+      t.timestamps
+    end
   end
 
   def down
