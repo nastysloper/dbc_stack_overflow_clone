a00732300f1a82697c286eea57ea250bdeb4adfa
TH <tylerhartland7@gmail.com>
2013-07-17 17:00:16 -0500

    change folder structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StackClone::Application.config.secret_token = '047c7ceccaadbafa0f5c151b3a60645cc2754eec3087c7d7840a138f89d52b398773c0c01bacde48e4915942a6eaafc770004177e70bb5451f9808e399326cdc'
