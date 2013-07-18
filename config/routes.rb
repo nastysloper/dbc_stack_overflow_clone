256e071d9a305205bada7102264c3fed9cd4c36b
TH <tylerhartland7@gmail.com>
2013-07-17 19:17:22 -0500

    quick dirty twitter oauth

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ StackClone::Application.routes.draw do
 
   resources :comments , :only => [:create, :edit, :destroy]
 
+  match 'user/auth', to: 'users#auth'
+
+  match '/sign_in', to: 'users#sign_in'
+  match '/sign_out', to: 'users#sign_out'
+
 end
