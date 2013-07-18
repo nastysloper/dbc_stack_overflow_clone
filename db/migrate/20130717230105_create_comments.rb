07bf6b0a8a8dee5dc9ec36b2e1ee4339b6182085
Torey Hickman <torey@toreyhickman.com>
2013-07-17 19:33:01 -0500

    comment model and migration

--- db/migrate/20130717230105_create_comments.rb
+++ db/migrate/20130717230105_create_comments.rb
@@ -1,5 +1,14 @@
 class CreateComments < ActiveRecord::Migration
   def up
+    create_table :comments do |t|
+      t.text :text
+      t.integer :author_id
+      t.belongs_to :event
+      t.integer :parent_id
+      t.boolean :endorsed, default: false
+
+      t.timestamps
+    end
   end
 
   def down
