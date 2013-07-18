5e977befec6ce1c5a4a806850220964478814f05
Patrick Villanueva <Patrick@Patricks-MacBook-Pro.local>
2013-07-18 16:01:06 -0500

    added variables for event views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,18 @@
 class EventsController < ApplicationController
 
   def index
+    @events = Event.all
   end
 
   def new
   end
 
   def create
+
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def edit
