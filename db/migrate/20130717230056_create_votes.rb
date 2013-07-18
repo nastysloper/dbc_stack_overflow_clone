1ed1e23193cc59f0f5b488822e3f9bfdc507a505
Torey Hickman <torey@toreyhickman.com>
2013-07-17 19:16:18 -0500

    votes model and migration

--- db/migrate/20130717230056_create_votes.rb
+++ db/migrate/20130717230056_create_votes.rb
@@ -1,5 +1,12 @@
 class CreateVotes < ActiveRecord::Migration
   def up
+    create_table :votes do |t|
+      t.integer :voter_id
+      t.belongs_to :comment
+      t.integer :value
+
+      t.timestamps
+    end
   end
 
   def down
