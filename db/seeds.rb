f94d0d66e165ca82ab0bc087ad93472720835a45
Torey Hickman <torey@toreyhickman.com>
2013-07-18 19:27:00 -0500

    seed data updated to generate users, events, attended_events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-Event.create(   :title => "Movie Night",
-				:description => "Monsters University",
-				:start => DateTime.now,
-				:end => DateTime.now,
-				:photo_file => "this is a photo",
-				:organizer_id => 1
-			)
+50.times do
+	User.create(twitter_handle: Faker::Internet.user_name)
+end
 
-Event.create(   :title => "Design Talk",
-				:description => "Photoshop Stuff",
-				:start => DateTime.now,
-				:end => DateTime.now,
-				:photo_file => "Photoshop logo",
-				:organizer_id => 1
-			)
+def all_users
+	User.all
+end
 
-Comment.create(	:text => "This is a new comment.",
-				:author_id => 1,
-				:event_id => 1,
-				:parent_id => 1
-				)
+def generate_start_date
+	"2013-#{random_number((8-10).to_a)}-#{random_number((1-31).to_a)} #{random_number((0-23).to_a)}:#{random_number((0-59).to_a)}:00"
+end
 
-Comment.create(	:text => "This is a another comment.",
-				:author_id => 1,
-				:event_id => 1,
-				:parent_id => 1
-				)
+def random_number(choices)
+	choices.sample
+end
+
+100.times do
+	start = generate_start_date
+	user = all_users.sample
+	user.created_events.create( { :title => Faker::Lorem.sentence([4,5,6,7].sample),
+																:description => Faker::Lorem.paragraph([1,2,3,4,5,6].sample)
+																:start => start,
+																:end =>  [nil, start + (1800-18000).to_a.sample].sample } )
+end
+
+def all_events
+	Event.all
+end
+
+150.times do
+	user = all_users.sample
+	event = all_events
+	user.attended_events << event unless event.organizer_id == user.id
+end
