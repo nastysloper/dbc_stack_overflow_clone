3130b594a468165aee5351542dee8d7522798162
TH <tylerhartland7@gmail.com>
2013-07-19 14:10:27 -0500

    can reply to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,10 @@
 class Comment < ActiveRecord::Base
 
-  attr_accessible :text, :author, :author_id
+  attr_accessible :text, :author, :author_id, :parent, :parent_id
   
   validates :text, :presence => true
 
+  belongs_to :parent, :class_name => "Comment"
   belongs_to :event
   belongs_to :author, :class_name => "User"
   has_many :replies, :class_name => "Comment", :foreign_key => :parent_id
